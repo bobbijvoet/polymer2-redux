bc6a13af1734c22a7b52068ab44b62880d8cb3f3
Bob Bijvoet <bob@frontmen.nl>
2016-11-24 08:13:05 +0100

    Add initial view message feature

--- src/reducers/inboxReducer.js
+++ src/reducers/inboxReducer.js
@@ -2,24 +2,42 @@ const initialState = {
     items: [{
         subject: 'Message 1',
         body: 'Message 1',
-        read: false
+        read: false,
+        id: 1
     }],
-    totalItems: 1
+    totalItems: 1,
+    selected: null
 };
+
+var id = 2;
+
 
 function cartReducer(state, action) {
     if (state === undefined) {
         return initialState;
     }
-
     switch (action.type) {
+
         case 'ADD_ITEM':
             action.item.read = false;
-            const items = [...state.items, action.item];
+            action.item.id = id++;
+            return Object.assign({}, state, {
+                items: [...state.items, action.item]
+            });
+
+        case 'OPEN_ITEM':
+            const items = [...state.items];
+            const index = items.findIndex((item) => {
+                return item.id === action.item.id;
+            });
+            const item = items[index];
+            item.read = true;
+
+            items.splice(index, 1, Object.assign({}, item));
 
             return Object.assign({}, state, {
-                items,
-                totalItems: items.length,
+                items: items,
+                selected: action.item.id
             });
 
         default:
